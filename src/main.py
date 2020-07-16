06cd2492fcb8c751640327151a55d8ad14397108
Anni Provietti <anniprovietti@gmail.com>
2020-07-16 19:14:41 +0000

    adiciona o main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*-
+""" Projeto sem descrição, (mude esta linha)."""
+
+"""codeauthor:: Anni Provietti <anniprovietti@gmail.com>
+
+
+    - teste github
+    - criar repositório
+    - clonar usando comando git
+    - comitar usando git
+
+Changelog
+
+.. versionadded::    20.07"""
+
+import random
+
+
+class Main:
+
+    """classe exemplo com shuffle"""
+    def _init_(self,versao):
+        #n=[0,2,3,4]
+        #random.shuffle(n,random,random)
+        print("versao:{}".format(versao))
+        #print(n)
+
+
+if __name__=="__main__":
+    Main(1)
+
